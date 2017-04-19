2ff20b1354c886efff5bb436d3037a0c6a4f9adb
ThisChessPlayer <jasonma5501@gmail.com>
2017-04-19 13:39:43 -0700

    added comments/documentation to make things easier

--- firmware/remote_firmware/remote_firmware.ino
+++ firmware/remote_firmware/remote_firmware.ino
@@ -1,12 +1,21 @@
+ /*****************************************************************************
+
+                                                       Author: Jason Ma
+                                                               Michael Gonzalez
+                                                       Date:   Apr 19 2017
+                                 remote_firmware
+
+ File Name:       remote_firmware.ino
+ Description:     Contains RedBoard code for remote to read from gimbals,
+                  display to LCD screen, and transmit radio signals.
+ *****************************************************************************/
+ 
 #include <radio.h>
 #include <serLCD.h>
 #include <Gimbal.h>
 
 #define GIMBAL_MAX 900 //not actually max value, but scales value
 #define GIMBAL_MIN 0   //not actually min value, but scales value
-
-//LCD
-serLCD mon;
 
 //Gimbal pins
 int left_x  = 0; //a0 yaw
@@ -18,7 +27,17 @@ int right_y = 3; //a3 pitch
 Gimbal left_gimbal (left_x, left_y);
 Gimbal right_gimbal (right_x, right_y);
 
+//LCD object
+serLCD mon;
+
 char * space = " ";
+
+ /********************************************************************
+ | Routine Name: setup
+ | File:         remote_firmware.ino
+ | 
+ | Description: Init gimbal, lcd display
+ ********************************************************************/
 void setup() {
   //init gimbal bounds
   right_gimbal.set_x_bounds(GIMBAL_MIN, GIMBAL_MAX);
@@ -30,46 +49,86 @@ void setup() {
   mon.clear();
   mon.display();
   mon.setBrightness(15); // can go from 1-30
-  //mon.autoscroll();
+  //mon.autoscroll(); //autoscroll is enabled by default it seems
 
   //init serial monitor
   //Serial.begin(115200);
   //Serial.print("Initialization complete");
 }
 
+ /********************************************************************
+ | Routine Name: loop
+ | File:         remote_firmware.ino
+ | 
+ | Description: Read from gimbals, display info on LCD screen, and
+ |              send commands to drone using radio.
+ ********************************************************************/
 void loop() {
   Vector2 l_g_v = left_gimbal.read();
   Vector2 r_g_v = right_gimbal.read();
   
-  //mon.clear();
+  display_gimbal_pos(l_g_v, r_g_v);
 
+  //TODO hook up motor and control it using throttle (l_g_v.y)
+
+  delay(100);
+}
+
+ /********************************************************************
+ | Routine Name: display_gimbal_pos
+ | File:         remote_firmware.ino
+ | 
+ | Description: Display gimbal values on LCD screen
+ |
+ | Parameter Descriptions:
+ | name               description
+ | ------------------ -----------------------------------------------
+ | l_g_v (Vector2)    left gimbal values
+ | r_g_v (Vector2)    right gimbal values
+ ********************************************************************/
+void display_gimbal_pos(Vector2 l_g_v, Vector2 r_g_v) {
+  //mon.clear() is slow, don't use it if possible
   mon.setCursor(0, 0);
+  mon.print("LX:");
   mon.print(l_g_v.x);
 
   if(l_g_v.x < 1000)
     mon.print(space);
     
-  mon.setCursor(0, 8);
+  mon.setCursor(1, 0);
+  mon.print("LY:");
   mon.print(l_g_v.y);
 
   if(l_g_v.y < 1000)
     mon.print(space);
 
-  mon.setCursor(1, 0);
+  mon.setCursor(0, 8);
+  mon.print("RX:");
   mon.print(r_g_v.x);
 
   if(r_g_v.x < 1000)
     mon.print(space);
     
   mon.setCursor(1, 8);
+  mon.print("RY:");
   mon.print(r_g_v.y);
 
   if(r_g_v.y < 1000)
     mon.print(space);
-  
-  //print_gimbal_pos(l_g_v, r_g_v);  
 }
 
+ /********************************************************************
+ | Routine Name: print_gimbal_pos
+ | File:         remote_firmware.ino
+ | 
+ | Description: Print gimbal values to serial monitor
+ |
+ | Parameter Descriptions:
+ | name               description
+ | ------------------ -----------------------------------------------
+ | l_g_v (Vector2)    left gimbal values
+ | r_g_v (Vector2)    right gimbal values
+ ********************************************************************/
 void print_gimbal_pos(Vector2 l_g_v, Vector2 r_g_v) {
   Serial.print(l_g_v.x);
   Serial.print("\t");
